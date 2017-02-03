875ec7df411059b7ea8edfcec7b22925a0ce9201
yanif <yanif@jhu.edu>
2017-02-03 19:40:08 +0000

    add trews-rest-api repo in storage

--- storage/main.tf
+++ storage/main.tf
@@ -21,6 +21,15 @@ resource "aws_ecr_repository" "trews_etl" {
   name = "trews-etl"
 }
 
+# Trews-rest API container registry.
+resource "aws_ecr_repository" "trews_rest_api" {
+  name = "trews-rest-api"
+}
+
 output "trews_etl_registry_url" {
   value = "${aws_ecr_repository.trews_etl.repository_url}"
+}
+
+output "trews_rest_api_registry_url" {
+  value = "${aws_ecr_repository.trews_rest_api.repository_url}"
 }
