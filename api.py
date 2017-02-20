189d4522bcbf37f45e1c9a1dbefb3aaa6c823e0d
Yanif Ahmad <yanif@jhu.edu>
2017-02-19 19:26:17 -0500

    Fixed override value logging, and reformatted override values as arrays

--- api.py
+++ api.py
@@ -96,11 +96,11 @@ class TREWSAPI(object):
                 else:
                     if 'value' in action:
                         as_min = action['range'] == 'min'
-                        value = ('{{ "lower": %(val)s }}' if as_min else '{{ "upper": %(val)s }} ') % {'val': action['value']}
+                        value = ('[{{ "lower": %(val)s }}]' if as_min else '[{{ "upper": %(val)s }}]') % {'val': action['value']}
                         logging.debug('override_criteria value: {} {}' % (action['actionName'], value))
                         query.override_criteria(eid, action['actionName'], value=value, is_met=action_is_met)
                     else:
-                        value = '{{ "lower": %(l)s, "upper": %(u)s }}' % {'l': action['values'][0], 'u': action['values'][1] }
+                        value = '[{{ "lower": %(l)s, "upper": %(u)s }}]' % {'l': action['values'][0], 'u': action['values'][1] }
                         logging.debug('override_criteria values: {} {}' % (action['actionName'], value))
                         query.override_criteria(eid, action['actionName'], value=value, is_met=action_is_met)
             #query.update_notifications()
@@ -155,7 +155,7 @@ class TREWSAPI(object):
         # TODO: set up the onset time
         for idx, row in criteria.iterrows():
             # update every criteria
-            logging.debug('getcriteria override_value: 'row['override_value']);
+            logging.debug('getcriteria override_value: ' + row['override_value']);
             criterion = {
                 "name": row['name'],
                 "is_met": row['is_met'],
