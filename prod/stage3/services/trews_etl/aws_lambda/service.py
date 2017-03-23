491ca54873bd57b859ba634b388a150b2ab677b1
Yanif Ahmad <yanif@jhu.edu>
2017-03-22 20:02:55 -0400

    Added aws env vars to ETL jobs from k8s secrets, to enable ETL to push CW metrics

--- prod/stage3/services/trews_etl/aws_lambda/service.py
+++ prod/stage3/services/trews_etl/aws_lambda/service.py
@@ -57,9 +57,9 @@ users:
             "name": "trews-etl",
             "image": os.environ["kube_image"],
             "env": [
-              {"name": "AWS_ACCESS_KEY_ID",            "value": os.environ["AWS_ACCESS_KEY_ID"]},
-              {"name": "AWS_SECRET_ACCESS_KEY",        "value": os.environ["AWS_SECRET_ACCESS_KEY"]},
-              {"name": "AWS_DEFAULT_REGION",           "value": os.environ["AWS_DEFAULT_REGION"]},
+              {"name": "AWS_ACCESS_KEY_ID",            "valueFrom": { "secretKeyRef": { "name": "aws-secrets", "key": "access_key_id"     } } },
+              {"name": "AWS_SECRET_ACCESS_KEY",        "valueFrom": { "secretKeyRef": { "name": "aws-secrets", "key": "secret_access_key" } } },
+              {"name": "AWS_DEFAULT_REGION",           "valueFrom": { "secretKeyRef": { "name": "aws-secrets", "key": "default_region"    } } },
               {"name": "db_host",                      "value": os.environ["db_host"]},
               {"name": "db_port",                      "value": os.environ["db_port"]},
               {"name": "db_name",                      "value": os.environ["db_name"]},
