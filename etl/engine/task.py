121c6815ca3e89eb53fea911073523ccd4e7f762
Yanif Ahmad <yanif@jhu.edu>
2017-03-24 18:48:29 -0400

    Added simple anonymous tasks by specifying run lambda

--- etl/engine/task.py
+++ etl/engine/task.py
@@ -4,6 +4,7 @@ class Task:
   def __init__(self, **kwargs):
     self.task_id = Task.num_tasks
     self.task_name = kwargs.get('name', '')
+    self.body = kwargs.get('run', None)
     Task.num_tasks += 1
 
   def run(self):
@@ -12,4 +13,6 @@ class Task:
 
     Task subclasses should override this method (and need not call this method)
     """
+    if body is not None:
+      return self.body()
     return []
