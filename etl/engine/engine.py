6982d1960dbfbacdc2140c7363deb0752e96288e
Yanif Ahmad <yanif@jhu.edu>
2017-03-24 18:45:06 -0400

    Simple serial engine & task implementation

---
+++ etl/engine/engine.py
@@ -0,0 +1,35 @@
+import os, logging
+from collections import deque
+
+class Engine:
+  """
+  ETL Engine, responsible for running ETL tasks.
+
+  For now, this is a simple serial engine.
+  """
+
+  def __init__(self, **kwargs):
+    # An engine identifier.
+    self.name = kwargs.get('name', 'etl-engine')
+
+    # A dictionary of tasks, indexed by task name.
+    self.tasks = {}
+
+    # A queue of tasks waiting to run.
+    self.run_queue = deque([])
+
+  def run(self):
+    while self.run_queue:
+      queue_ids = [task.task_name if task.task_name else task.task_id for task in self.run_queue]
+      logging.debug('Queue {}'.format(str(queue_ids)))
+
+      task = self.run_queue.popleft()
+
+      logging.info('Running [{}]{}'.format(task.task_id, task.task_name))
+      next_tasks = task.run()
+      for t in next_tasks:
+        logging.debug('Scheduled [{}]{}'.format(t.task_id, t.task_name))
+        self.schedule(t)
+
+  def schedule(self, task);
+    self.run_queue.append(task)
---
+++ etl/engine/task.py
@@ -0,0 +1,15 @@
+class Task:
+  num_tasks = 0
+
+  def __init__(self, **kwargs):
+    self.task_id = Task.num_tasks
+    self.task_name = kwargs.get('name', '')
+    Task.num_tasks += 1
+
+  def run(self):
+    """
+    Returns a list of next tasks to run.
+
+    Task subclasses should override this method (and need not call this method)
+    """
+    return []
