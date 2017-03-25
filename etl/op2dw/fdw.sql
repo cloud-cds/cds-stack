b93020f7501c6a7135ba745a9deb4721e6c13c6d
Yanif Ahmad <yanif@jhu.edu>
2017-03-25 18:21:27 -0400

    Minor updates for fdw server/user mapping

--- etl/op2dw/fdw.sql
+++ etl/op2dw/fdw.sql
@@ -1,13 +1,15 @@
 --
 -- FDW setup for data warehouse Postgres instance.
 -- This will pull tables from the operational DB into the DW.
-CREATE EXTENSION postgres_fdw;
-CREATE EXTENSION dblink;
+CREATE EXTENSION IF NOT EXISTS postgres_fdw;
+CREATE EXTENSION IF NOT EXISTS dblink;
 
+DROP SERVER IF EXISTS opsdx_opdb;
 CREATE SERVER opsdx_opdb
   FOREIGN DATA WRAPPER postgres_fdw
   OPTIONS (host '@@RDBHOST@@', port '5432', dbname '@@RDBNAME@@', sslmode 'require');
 
+DROP USER MAPPING IF EXISTS FOR @@LDBUSER@@ SERVER opsdx_opdb;
 CREATE USER MAPPING FOR @@LDBUSER@@
   SERVER foreign_server
   OPTIONS (user '@@RDBUSER@@', password '@@RDBPW@@');
