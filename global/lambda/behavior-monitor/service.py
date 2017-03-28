e22503041565414b552a4a828b19593a0929c579
Yanif Ahmad <yanif@jhu.edu>
2017-03-28 12:28:53 -0400

    Added a behavior monitor lambda template

---
+++ global/lambda/behavior-monitor/service.py
@@ -0,0 +1,24 @@
+import os
+from sqlalchemy import create_engine
+
+def handler(event, context):
+  host          = os.environ['db_host']
+  port          = os.environ['db_port']
+  db            = os.environ['db_name']
+  user          = os.environ['db_user']
+  pw            = os.environ['db_password']
+  conn_str      = 'postgresql://{}:{}@{}:{}/{}'.format(user, password, host, port, db)
+  db_engine     = create_engine(conn_str)
+
+  get_trews_threshold_sql = \
+    '''
+    select value from trews_parameters
+    where name = 'trews_threshold'
+    '''
+
+  conn = db_engine.connect()
+  result = conn.execute(get_trews_threshold_sql)
+  conn.close()
+  row = result.fetchone()
+  print("trews threshold %s" % row['value'])
+
