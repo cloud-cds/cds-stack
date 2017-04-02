29b125be3cf48b1cf12d6377766eb8b37be88901
Andong Zhan <zad522@gmail.com>
2017-04-01 22:43:58 -0400

    create sql to extract and load clarity data from testing

--- ccda643/ccda_all_queries_phi.sql
+++ ccda643/ccda_all_queries_phi.sql
@@ -14,52 +14,55 @@ FROM
   LEFT JOIN CLARITY.dbo.CLARITY_ADT discharge ON Medicalxferin.DIS_EVENT_ID = discharge.EVENT_ID
   WHERE
   --Age is greater than 15 at hospital admission
-  datediff(year, patient.birth_date, PAT_ENC_HSP_1.HOSP_ADMSN_TIME) + CASE
-      WHEN MONTH(PAT_ENC_HSP_1.HOSP_ADMSN_TIME) < month(patient.birth_date)
-        THEN - 1
-      WHEN month(PAT_ENC_HSP_1.HOSP_ADMSN_TIME) = month(patient.birth_date)
-        AND day(PAT_ENC_HSP_1.HOSP_ADMSN_TIME) < day(patient.birth_date)
-        THEN - 1
-      ELSE 0
-      END >= 15
+  -- datediff(year, patient.birth_date, PAT_ENC_HSP_1.HOSP_ADMSN_TIME) + CASE
+  --     WHEN MONTH(PAT_ENC_HSP_1.HOSP_ADMSN_TIME) < month(patient.birth_date)
+  --       THEN - 1
+  --     WHEN month(PAT_ENC_HSP_1.HOSP_ADMSN_TIME) = month(patient.birth_date)
+  --       AND day(PAT_ENC_HSP_1.HOSP_ADMSN_TIME) < day(patient.birth_date)
+  --       THEN - 1
+  --     ELSE 0
+  --     END >= 15
   -- EVENT TYPE FOR TRANSFER INTO MEDICAL UNIT IS ADMISSION, TRANSFER OR DISCHARGE
-    AND Medicalxferin.EVENT_TYPE_C < 5
+    -- AND Medicalxferin.EVENT_TYPE_C < 5
   -- ENSURE PATIENT WAS ADMITTED TO ONE OF THE HOWARD COUNTY UNITS YOU ARE STUDYING
-    AND Medicalxferin.DEPARTMENT_ID IN (
-      '110300814'
-    ,'110300855'
-    ,'110300270'
-    ,'110300140'
-    ,'110300280'
-    ,'110300180'
-    ,'110300110'
-    ,'110300120'
-    ,'110300130'
-    ,'110300170'
-      )
+    -- AND
+    Medicalxferin.DEPARTMENT_ID like '1103%'
+    -- IN (
+    --   '110300814'
+    -- ,'110300855'
+    -- ,'110300270'
+    -- ,'110300140'
+    -- ,'110300280'
+    -- ,'110300180'
+    -- ,'110300110'
+    -- ,'110300120'
+    -- ,'110300130'
+    -- ,'110300170'
+    --   )
     -- ADT EVENT WAS NOT CANCELED
-    AND Medicalxferin.EVENT_SUBTYPE_C IN (
-      1
-      ,3
-      )
+    -- AND Medicalxferin.EVENT_SUBTYPE_C IN (
+    --   1
+    --   ,3
+    --   )
     -- Either they came right from the HCGH ED or were a direct admit to HCGH
-    AND (
-      (edxferout.EVENT_ID IS NULL and Medicalxferin.EVENT_TYPE_C = 1)
-      OR (
-        EDxferout.EVENT_SUBTYPE_C IN (
-          1
-          ,3
-          )
-        AND edxferout.DEPARTMENT_ID IN (
-          '110300470'
-          ,'110300460'
-          )
-        )
-      ) AND
-    --no patients that are still present in hospital
-    HOSP_DISCH_TIME IS NOT NULL
+    -- AND (
+    --   (edxferout.EVENT_ID IS NULL and Medicalxferin.EVENT_TYPE_C = 1)
+    --   OR (
+    --     EDxferout.EVENT_SUBTYPE_C IN (
+    --       1
+    --       ,3
+    --       )
+    --     AND edxferout.DEPARTMENT_ID IN (
+    --       '110300470'
+    --       ,'110300460'
+    --       )
+    --     )
+    --   ) AND
+    -- for patients that are still present in hospital
+    AND HOSP_DISCH_TIME IS NULL
     --admitted between the dates in your cohort
-    AND HOSP_ADMSN_TIME BETWEEN '2017-03-25' AND  '2017-04-02'
+    AND
+    HOSP_ADMSN_TIME BETWEEN '2017-03-25' AND  '2017-04-02'
 ) A (csn, pat_id, pat_mrn_id);
 
 -- DO NOT RUN THIS CODE UNTIL YOU'VE ALTERED THE DATES IN THE COHORT, OTHERWISE IT WILL CREATE DUPLICATE RECORDS!!!
@@ -80,50 +83,51 @@ FROM
   WHERE
       IDENTITY_ID.line = 1
   --Age is greater than 15 at hospital admission
-  AND datediff(year, patient.birth_date, PAT_ENC_HSP_1.HOSP_ADMSN_TIME) + CASE
-      WHEN MONTH(PAT_ENC_HSP_1.HOSP_ADMSN_TIME) < month(patient.birth_date)
-        THEN - 1
-      WHEN month(PAT_ENC_HSP_1.HOSP_ADMSN_TIME) = month(patient.birth_date)
-        AND day(PAT_ENC_HSP_1.HOSP_ADMSN_TIME) < day(patient.birth_date)
-        THEN - 1
-      ELSE 0
-      END >= 15
-  -- EVENT TYPE FOR TRANSFER INTO MEDICAL UNIT IS ADMISSION, TRANSFER OR DISCHARGE
-    AND Medicalxferin.EVENT_TYPE_C < 5
-  -- ENSURE PATIENT WAS ADMITTED TO ONE OF THE HOWARD COUNTY UNITS YOU ARE STUDYING
-    AND Medicalxferin.DEPARTMENT_ID IN (
-      '110300814'
-    ,'110300855'
-    ,'110300270'
-    ,'110300140'
-    ,'110300280'
-    ,'110300180'
-    ,'110300110'
-    ,'110300120'
-    ,'110300130'
-    ,'110300170'
-      )
-    -- ADT EVENT WAS NOT CANCELED
-    AND Medicalxferin.EVENT_SUBTYPE_C IN (
-      1
-      ,3
-      )
-    -- Either they came right from the HCGH ED or were a direct admit to HCGH
-    AND (
-      (edxferout.EVENT_ID IS NULL and Medicalxferin.EVENT_TYPE_C = 1)
-      OR (
-        EDxferout.EVENT_SUBTYPE_C IN (
-          1
-          ,3
-          )
-        AND edxferout.DEPARTMENT_ID IN (
-          '110300470'
-          ,'110300460'
-          )
-        )
-      ) AND
-    --no patients that are still present in hospital
-    HOSP_DISCH_TIME IS NOT NULL
+  -- AND datediff(year, patient.birth_date, PAT_ENC_HSP_1.HOSP_ADMSN_TIME) + CASE
+  --     WHEN MONTH(PAT_ENC_HSP_1.HOSP_ADMSN_TIME) < month(patient.birth_date)
+  --       THEN - 1
+  --     WHEN month(PAT_ENC_HSP_1.HOSP_ADMSN_TIME) = month(patient.birth_date)
+  --       AND day(PAT_ENC_HSP_1.HOSP_ADMSN_TIME) < day(patient.birth_date)
+  --       THEN - 1
+  --     ELSE 0
+  --     END >= 15
+  -- -- EVENT TYPE FOR TRANSFER INTO MEDICAL UNIT IS ADMISSION, TRANSFER OR DISCHARGE
+  --   AND Medicalxferin.EVENT_TYPE_C < 5
+  -- -- ENSURE PATIENT WAS ADMITTED TO ONE OF THE HOWARD COUNTY UNITS YOU ARE STUDYING
+  AND Medicalxferin.DEPARTMENT_ID like '1103%'
+  -- IN (
+  --     '110300814'
+  --   ,'110300855'
+  --   ,'110300270'
+  --   ,'110300140'
+  --   ,'110300280'
+  --   ,'110300180'
+  --   ,'110300110'
+  --   ,'110300120'
+  --   ,'110300130'
+  --   ,'110300170'
+  --     )
+  --   -- ADT EVENT WAS NOT CANCELED
+  --   AND Medicalxferin.EVENT_SUBTYPE_C IN (
+  --     1
+  --     ,3
+  --     )
+  --   -- Either they came right from the HCGH ED or were a direct admit to HCGH
+  --   AND (
+  --     (edxferout.EVENT_ID IS NULL and Medicalxferin.EVENT_TYPE_C = 1)
+  --     OR (
+  --       EDxferout.EVENT_SUBTYPE_C IN (
+  --         1
+  --         ,3
+  --         )
+  --       AND edxferout.DEPARTMENT_ID IN (
+  --         '110300470'
+  --         ,'110300460'
+  --         )
+  --       )
+  --     ) AND
+  --   --no patients that are still present in hospital
+    AND HOSP_DISCH_TIME IS NULL
     --admitted between the dates in your cohort
     AND HOSP_ADMSN_TIME BETWEEN '2017-03-25' AND  '2017-04-02'
 ) A (pat_id, pat_mrn_id, identity_id);
