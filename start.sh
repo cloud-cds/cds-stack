7cdb99aad6d4108e27b9507213583d140ed6fea9
Mike Peven <mpeven@gmail.com>
2017-04-07 10:27:54 -0400

    finish flamegraph script and add svg builder

--- start.sh
+++ start.sh
@@ -1,13 +1,18 @@
 gunicorn -b localhost:8001 trews:app &
 GUNICORN_PID=$!
+CUR_DIR=$(pwd)
 echo $GUNICORN_PID
 while true
 do
   CHILD_PID=$(pgrep -P $GUNICORN_PID)
   if kill -0 $CHILD_PID > /dev/null 2>&1
   then
-    OUTFILE=$(date -u +pyflame_out/%Y_%m_%H_%M_%S.out)
-    sudo pyflame -s 10 -o $OUTFILE -x $CHILD_PID
+    DATE=$(date -u +%Y_%m_%d)
+    FULL_DATE=$(date -u +%Y_%m_%d_%H_%M_%S)
+    OUTFILE=$CUR_DIR/$FULL_DATE.svg
+    sudo pyflame -s 30 -x $CHILD_PID | $CUR_DIR/utils/flamegraph.pl > $OUTFILE
+    aws s3 cp $OUTFILE s3://opsdx-flamegraph-dev/$DATE/$FULL_DATE.svg`
+    rm $OUTFILE
   else
     echo "No gunicorn workers"
     sleep 10
