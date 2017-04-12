53df129735227de8991a38bd52765687ff25bceb
Andong Zhan <zad522@gmail.com>
2017-04-11 21:39:15 -0400

    fixed unit error in sodium

--- etl/transforms/primitives/row/transform.py
+++ etl/transforms/primitives/row/transform.py
@@ -889,12 +889,13 @@ def convert_to_mmol(entry, log):
     tsp = entry[2]
     dose = entry[3]
     unit = entry[4]
+    unit = unit.lower()
     if dose is None or dose == 'Test not performed':
         return None
-    log_assert(log, unit == 'mEq/L' or unit == 'mmol/L', "Unknown unit %s" % unit   )
-    if unit == 'mmol/L':
+    log_assert(log, unit == 'meq/l' or unit == 'mmol/l', "Unknown unit %s" % unit   )
+    if unit == 'mmol/l':
         return [float(dose), confidence.NO_TRANSFORM]
-    elif unit == 'mEq/L':
+    elif unit == 'meq/l':
         return [float(dose), confidence.UNIT_TRANSFORMED]
 
 def convert_vent_to_binary(entry, log):
