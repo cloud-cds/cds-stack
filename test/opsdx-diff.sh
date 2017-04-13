4586d047e6c885a60ba2929de3d706b80cb5f747
Andong Zhan <zad522@gmail.com>
2017-04-12 20:43:03 -0400

    runnable meta data compare

--- test/opsdx-diff.sh
+++ test/opsdx-diff.sh
@@ -10,7 +10,7 @@ function setup() {
   local db_host=$1
   local db_name=$2
   echo "Setup database $db_host $db_name"
-  pg_dump -h $db_host -U $db_user -d $db_name -p $db_port --schema-only > $db_name.sql
+  pg_dump -h $db_host -U $db_user -d $db_name -p $db_port --schema='public' --schema-only > $db_name.sql
   dropdb -h $db_host -U $db_user -p $db_port schema_$db_name
   createdb -h $db_host -U $db_user -p $db_port -T template0 schema_$db_name
   psql -h $db_host -U $db_user -d schema_$db_name -p $db_port -f $db_name.sql
