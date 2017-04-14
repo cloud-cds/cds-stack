b014e1c5b986309421880a84666eddadc8d707aa
Andong Zhan <zad522@gmail.com>
2017-04-13 21:31:54 -0400

    specify db_host_2

--- test/create_db_epic2op_vs_c2dw.sh
+++ test/create_db_epic2op_vs_c2dw.sh
@@ -1,21 +1,22 @@
-db_host=$1
+db_host_1=$1
 db1=$2
-db2=$3
-model_id=$4
+db_host_2=$3
+db2=$4
+model_id=$5
 
 dashan_db_path=$(dirname `pwd`)
 export PGPASSWORD=$db_password
 
 echo "create database" $db1
 cd $dashan_db_path/ops
-sh create_db.sh $db_host $db1
+sh create_db.sh $db_host_1 $db1
 
 echo "create database" $db2
 cd $dashan_db_path/dw
-sh create_db.sh $db_host $db2 $model_id
+sh create_db.sh $db_host_2 $db2 $model_id
 
 echo "create dblink" $db2
 cd ${dashan_db_path}/test
-sed -e "s/@@RDBHOST@@/$db_host/" -e "s/@@RDBPORT@@/$db_port/" -e "s/@@RDBNAME@@/$db1/" -e "s/@@RDBUSER@@/$db_user/" -e "s/@@RDBPW@@/$db_password/" -e "s/@@LDBUSER@@/$db_user/" -e "s/@@db_to_test@@/$db1/" fdw.sql > fdw.$db2.sql
-psql -h $db_host -U $db_user -d $db2 -p $db_port -f fdw.$db2.sql
+sed -e "s/@@RDBHOST@@/$db_host_1/" -e "s/@@RDBPORT@@/$db_port/" -e "s/@@RDBNAME@@/$db1/" -e "s/@@RDBUSER@@/$db_user/" -e "s/@@RDBPW@@/$db_password/" -e "s/@@LDBUSER@@/$db_user/" -e "s/@@db_to_test@@/$db1/" fdw.sql > fdw.$db2.sql
+psql -h $db_host_2 -U $db_user -d $db2 -p $db_port -f fdw.$db2.sql
 rm fdw.$db2.sql
