406857b7d93a1cb95ff387cca7f685c13cea62fc
Andong Zhan <zad522@gmail.com>
2017-04-26 11:39:31 -0400

    use env for dataset_id

--- ml/run-ml.sh
+++ ml/run-ml.sh
@@ -1,4 +1,7 @@
+echo "start trews-ml"
+echo "installing R packages"
 R CMD BATCH /ml/install-packages.R
+echo "R packages installed"
 alias python=python3.6
 python ml/generate_input.py
-python ml/main.py input/1.pkl
+python ml/main.py input/$dataset_id.pkl
