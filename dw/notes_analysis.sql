9eb8142e851d31e868a2fc859b4b47041277c8c1
Yanif Ahmad <yanif@jhu.edu>
2017-05-04 18:14:42 -0400

    Revised cdm_notes analysis functions to abstract over Clarity and Mulesoft data representations

--- dw/notes_analysis.sql
+++ dw/notes_analysis.sql
@@ -233,16 +233,41 @@ END; $function$;
 
 
 --------------------------------------------------------
--- Notes processing on cdm tables, for Clarity datasets.
+-- Notes processing on cdm tables.
 --------------------------------------------------------
+
+--
+-- Helper functions to abstract over Clarity and Mulesoft data representations.
+
+create or replace function note_date(dates json) returns timestamptz
+  language plpgsql
+as $func$ begin
+  return
+    case json_typeof(dates)
+      when 'array' then (select min(dt->>'Date') from json_array_elements(dates) D(dt) where dt->>'DateType' = 'NoteDate')::timestamp at time zone 'UTC'
+      when 'object' then (dates->>'create_instant_dttm')::timestamp at time zone 'UTC'
+      else null
+    end;
+end; $func$;
+
+create or replace function note_provider_type(providers json) returns text
+  language plpgsql
+as $func$ begin
+  return
+    case json_typeof(providers)
+      when 'array' then providers->0->>'ProviderType'
+      when 'object' then providers->>'AuthorType'
+      else null
+    end;
+end; $func$;
 
 -----------------------------------------
 -- Candidate search via positive version
 
-drop function if exists match_cdm_infection_positives(integer, integer);
+drop function if exists match_cdm_infection_positives(integer, integer, integer);
 
-create or replace function match_cdm_infection_positives(num_matches integer, match_offset integer)
- RETURNS table(pat_id text, start_ts timestamptz, note text)
+create or replace function match_cdm_infection_positives(this_dataset_id integer, num_matches integer, match_offset integer)
+ RETURNS table(pat_id varchar(50), start_ts timestamptz, note text)
  LANGUAGE plpgsql
 AS $function$
 DECLARE
@@ -255,13 +280,14 @@ BEGIN
   select '(' || array_to_string(array_agg(keyword), '|') || ')' into grouped_positive
   from infection_keywords;
   match_query :=
-     'select pat_id, (dates->>''create_instant_dttm'')::timestamp at time zone ''UTC'' as start_ts,'
+     'select pat_id, note_date(dates) as start_ts,'
   || '       regexp_replace(array_to_string(array_agg(note_body), E''\n''), E''' || grouped_positive || ''', E''__MATCH__\\1'', ''g'') as note'
   || ' from cdm_notes'
-  || ' where note_body ~ E''' || positive || ''''
-  || ' and providers->>''AuthorType'' <> ''Pharmacist'''
-  || ' group by pat_id, dates->>''create_instant_dttm'''
-  || ' order by pat_id, dates->>''create_instant_dttm'' limit ' || num_matches::text || ' offset ' || match_offset::text;
+  || ' where dataset_id = coalesce(' || this_dataset_id::text || '::integer, dataset_id)'
+  || ' and note_body ~ E''' || positive || ''''
+  || ' and note_provider_type(providers) <> ''Pharmacist'''
+  || ' group by pat_id, note_date(dates)'
+  || ' order by pat_id, note_date(dates) limit ' || num_matches::text || ' offset ' || match_offset::text;
   --raise notice 'query %', match_query;
   return query execute match_query;
 END; $function$;
@@ -277,10 +303,10 @@ END; $function$;
 --- Does attribution matter? I.e., should something mentioned in nurse's note trigger critieria
 ---  or restrict this to physician notes?
 
-drop function if exists match_cdm_infections(text,integer,integer);
+drop function if exists match_cdm_infections(text,integer,integer,integer);
 
-create or replace function match_cdm_infections(this_pat_id text, rows_before integer, rows_after integer)
- RETURNS table(pat_id text, start_ts timestamptz, ngram text)
+create or replace function match_cdm_infections(this_pat_id text, this_dataset_id integer, rows_before integer, rows_after integer)
+ RETURNS table(pat_id varchar(50), start_ts timestamptz, ngram text)
  LANGUAGE plpgsql
 AS $function$
 DECLARE
@@ -296,7 +322,7 @@ BEGIN
   select array_to_string(array_agg(N.keyword || E'\\\\s*' || I.keyword), '|') into negative
   from infection_keywords I, negation_keywords N;
   match_query :=
-     ' select pat_id, start_ts at time zone ''UTC'', array_to_string(ngram_arr, '' '') as ngram  '
+     ' select pat_id, start_ts, array_to_string(ngram_arr, '' '') as ngram  '
   || ' from ('
   || '   select DOCS.pat_id, DOCS.start_ts, '
   || '          array_agg(W.word) over ( ROWS BETWEEN ' || rows_before::text || ' PRECEDING AND ' || rows_after::text || ' FOLLOWING ) as ngram_arr'
@@ -304,16 +330,19 @@ BEGIN
   || '     select pat_id, start_ts, '
   || '           regexp_split_to_array(regexp_replace(body, ''' || grouped_positive || ''', E''__MATCH__\\1'', ''g''), E''\\s+'') as words'
   || '     from ('
-  || '       select pat_id, dates->>''create_instant_dttm'' as start_ts,'
+  || '       select pat_id, note_date(dates) as start_ts,'
   || '              regexp_replace(note_body, E''' || negative || ''', ''NEGATED_PHRASE'', ''g'') as body'
   || '       from cdm_notes'
-  || '       where pat_id = coalesce(E''' || this_pat_id || ''', pat_id)'
-  || '       and providers->>''AuthorType'' <> ''Pharmacist'''
+  || '       where pat_id = coalesce(' || coalesce('E''' || this_pat_id || '''', 'null::text') || ', pat_id)'
+  || '       and dataset_id = coalesce(' || this_dataset_id::text || '::integer, dataset_id)'
+  || '       and note_provider_type(providers) <> ''Pharmacist'''
   || '     ) NEG'
   || '     where body ~ ''' || positive || ''''
   || '   ) DOCS, lateral unnest(words) W(word)'
   || ' ) NGRAMS'
-  || ' where ( ngram_arr[4] like ''%__MATCH__%'') or ( array_length(ngram_arr, 1) < ' || (rows_before+rows_after+1)::text || ' and (select count(*) from unnest(ngram_arr) W(word) where word like ''%__MATCH__%'' ) > 0 )'
+  || ' where ( ngram_arr[4] like ''%__MATCH__%'') '
+  || ' or ( array_length(ngram_arr, 1) < ' || (rows_before+rows_after+1)::text
+  ||       ' and (select count(*) from unnest(ngram_arr) W(word) where word like ''%__MATCH__%'' ) > 0 )'
   || ' order by pat_id, start_ts';
   --raise notice 'query %', match_query;
   return query execute match_query;
@@ -323,10 +352,10 @@ END; $function$;
 -------------------------
 -- Negation removal
 
-drop function if exists match_cdm_infection_negatives(text);
+drop function if exists match_cdm_infection_negatives(text, integer);
 
-create or replace function match_cdm_infection_negatives(this_pat_id text)
- RETURNS table(pat_id text, start_ts timestamptz, note text)
+create or replace function match_cdm_infection_negatives(this_pat_id text, this_dataset_id integer)
+ RETURNS table(pat_id varchar(50), start_ts timestamptz, note text)
  LANGUAGE plpgsql
 AS $function$
 DECLARE
@@ -336,12 +365,13 @@ BEGIN
   select array_to_string(array_agg(N.keyword || E'\\\\s*' || I.keyword), '|') into negative
   from infection_keywords I, negation_keywords N;
   match_query :=
-     'select pat_id, (dates->>''create_instant_dttm'')::timestamp at time zone ''UTC'' as start_ts,'
+     'select pat_id, note_date(dates) as start_ts,'
   || '       regexp_replace(note_body, E''' || negative || ''', ''NEGATED_PHRASE'', ''g'') as body'
   || ' from cdm_notes'
-  || ' where pat_id = coalesce(E''' || this_pat_id || ''', pat_id)'
-  || ' and providers->>''AuthorType'' <> ''Pharmacist'''
-  || ' order by dates->>''create_instant_dttm''';
+  || ' where pat_id = coalesce(' || coalesce('E''' || this_pat_id || '''', 'null::text') || ', pat_id)'
+  || ' and dataset_id = coalesce(' || this_dataset_id::text || '::integer, dataset_id)'
+  || ' and note_provider_type(providers) <> ''Pharmacist'''
+  || ' order by note_date(dates)';
   --raise notice 'query %', match_query;
   return query execute match_query;
 END; $function$;
@@ -350,10 +380,10 @@ END; $function$;
 -------------------------------------
 -- Batch version
 
-drop function if exists match_cdm_infections_multi(text[],integer,integer);
+drop function if exists match_cdm_infections_multi(text[],integer,integer,integer);
 
-create or replace function match_cdm_infections_multi(pat_ids text[], rows_before integer, rows_after integer)
- RETURNS table(pat_id integer, start_ts timestamptz, note text)
+create or replace function match_cdm_infections_multi(pat_ids text[], this_dataset_id integer, rows_before integer, rows_after integer)
+ RETURNS table(pat_id varchar(50), start_ts timestamptz, note text)
  LANGUAGE plpgsql
 AS $function$
 DECLARE
@@ -376,7 +406,7 @@ BEGIN
     select * from unnest(pats_ids) E(match_pat_id);
 
   match_query :=
-     ' select pat_id, start_ts at time zone ''UTC'', array_to_string(ngram_arr, '' '') as ngram  '
+     ' select pat_id, start_ts, array_to_string(ngram_arr, '' '') as ngram  '
   || ' from ('
   || '   select DOCS.pat_id, DOCS.start_ts, '
   || '          array_agg(W.word) over ( ROWS BETWEEN ' || rows_before::text || ' PRECEDING AND ' || rows_after::text || ' FOLLOWING ) as ngram_arr'
@@ -384,11 +414,12 @@ BEGIN
   || '     select pat_id, start_ts, '
   || '           regexp_split_to_array(regexp_replace(body, ''' || grouped_positive || ''', E''__MATCH__\\1'', ''g''), E''\\s+'') as words'
   || '     from ('
-  || '       select pat_id, dates->>''create_instant_dttm'' as start_ts,'
+  || '       select pat_id, note_date(dates) as start_ts,'
   || '              regexp_replace(note_body, E''' || negative || ''', ''NEGATED_PHRASE'') as body'
   || '       from cdm_notes'
   || '       where pat_id in (select * from match_pats)'
-  || '       and providers->>''AuthorType'' <> ''Pharmacist'''
+  || '       and dataset_id = coalesce(' || this_dataset_id::text || '::integer, dataset_id)'
+  || '       and note_provider_type(providers) <> ''Pharmacist'''
   || '     ) NEG'
   || '     where body ~ ''' || positive || ''''
   || '   ) DOCS, lateral unnest(words) W(word)'
