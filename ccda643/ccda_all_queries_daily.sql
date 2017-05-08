a620c3ea83efdf05fa9e724234441e090ba72106
Andong Zhan <zad522@gmail.com>
2017-05-08 17:01:13 -0400

    add med dict

--- ccda643/ccda_all_queries_daily.sql
+++ ccda643/ccda_all_queries_daily.sql
@@ -765,3 +765,23 @@ GO
 SET NOCOUNT ON
 select DISTINCT proc_id, proc_name, proc_code FROM CLARITY..CLARITY_EAP
 GO
+
+:OUT \\Client\H$\Downloads\clarity\med_dict.rpt
+SET NOCOUNT ON
+select
+MEDICATION_ID,
+meds.name,
+GENERIC_NAME,
+STRENGTH,
+form,
+route,
+meds.PHARM_CLASS_C,
+pharmClass.name pharm_class_name,
+pharmClass.title pharm_class_title,
+meds.THERA_CLASS_C,
+thera.name threa_class_name,
+thera.title threa_class_title
+ from clarity.dbo.CLARITY_MEDICATION meds
+ LEFT  JOIN dbo.ZC_PHARM_CLASS pharmClass ON pharmClass.PHARM_CLASS_C = meds.PHARM_CLASS_C
+ LEFT JOIN dbo.ZC_THERA_CLASS thera ON thera.THERA_CLASS_C = meds.THERA_CLASS_C
+GO
