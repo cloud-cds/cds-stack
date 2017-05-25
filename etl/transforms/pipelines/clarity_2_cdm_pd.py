13e6248442d2f0967fb1eda97d142daad56e831a
Andong Zhan <zad522@gmail.com>
2017-05-25 12:28:53 -0400

    remove all nan values from pull_med_order function

--- etl/transforms/pipelines/clarity_2_cdm_pd.py
+++ etl/transforms/pipelines/clarity_2_cdm_pd.py
@@ -184,7 +184,8 @@ async def pull_medication_admin(connection, dataset_id, fids, log, is_plan, clar
 
   if not is_plan:
     for idx, row in ma.iterrows():
-      await load_row.upsert_t(connection, [row['enc_id'], row['tsp'], row['fid'], str(row['dose_value']), row['confidence']], dataset_id=dataset_id)
+      if str(row['dose_value']) != 'nan':
+        await load_row.upsert_t(connection, [row['enc_id'], row['tsp'], row['fid'], str(row['dose_value']), row['confidence']], dataset_id=dataset_id)
     log.info('Medication Order Write complete')
   else:
     log.info('Medication Order Upsert skipped, due to plan mode')
