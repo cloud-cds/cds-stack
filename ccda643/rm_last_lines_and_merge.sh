276b9a2c49a3fa6345da12e35dc0d4f916e372c7
Andong Zhan <zad522@gmail.com>
2017-06-13 09:31:18 -0400

    uploaded chuck based queries

---
+++ ccda643/rm_last_lines_and_merge.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+mkdir -p $2
+cd $1
+mv flowsheet_dict.rpt ../$2/
+mv lab_dict.rpt ../$2/
+mv lab_proc.rpt ../$2/
+for filename in *; do
+        echo $filename
+        echo $(echo $filename | cut -f1,3 -d.)
+        if [[ $filename == *"hist"* ]]; then
+                head -n -2 $filename >> ../$2/$(echo $filename | cut -f1,3 -d.)
+        else
+                head -n -1 $filename >> ../$2/$(echo $filename | cut -f1,3 -d.)
+        fi
+done
