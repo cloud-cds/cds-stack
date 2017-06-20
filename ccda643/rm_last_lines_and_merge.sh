e8ba62f1acf00c0898c3bec6e4939764a1f17d14
Andong Zhan <zad522@gmail.com>
2017-06-20 11:57:19 -0400

    minor change

--- ccda643/rm_last_lines_and_merge.sh
+++ ccda643/rm_last_lines_and_merge.sh
@@ -5,11 +5,11 @@ mv flowsheet_dict.rpt ../$2/
 mv lab_dict.rpt ../$2/
 mv lab_proc.rpt ../$2/
 for filename in *; do
-        echo $filename
-        echo $(echo $filename | cut -f1,3 -d.)
-        if [[ $filename == *"hist"* ]]; then
-                head -n -2 $filename >> ../$2/$(echo $filename | cut -f1,3 -d.)
-        else
-                head -n -1 $filename >> ../$2/$(echo $filename | cut -f1,3 -d.)
-        fi
+  echo $filename
+  echo $(echo $filename | cut -f1,3 -d.)
+  if [[ $filename == *"hist"* ]]; then
+          head -n -2 $filename >> ../$2/$(echo $filename | cut -f1,3 -d.)
+  else
+          head -n -1 $filename >> ../$2/$(echo $filename | cut -f1,3 -d.)
+  fi
 done
