59a2a83c45a6bbaa42c98f7270621e65b120c4a3
Andong Zhan <zad522@gmail.com>
2017-08-10 14:39:42 -0400

    fix logging without printing too much info in request error

--- etl/io_config/jhapi.py
+++ etl/io_config/jhapi.py
@@ -56,7 +56,7 @@ class JHAPIConfig:
               return await response.json()
         except Exception as e:
           if i < request_attempts - 1 and str(e) != 'Session is closed' and not e.errno in (104):
-            logging.error("Request Error Caught for URL {}, setting {}, retrying... {} times".format(url, setting,i+1))
+            logging.error("Request Error Caught for URL {}, retrying... {} times".format(url,i+1))
             logging.exception(e)
             sleep(0.5)
           else:
