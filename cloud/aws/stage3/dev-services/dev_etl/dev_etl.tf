2ddf7bb7645476f51a20ea8751c03c52c527e9ae
Andong Zhan <zad522@gmail.com>
2017-09-04 14:59:39 -0400

    update dev_etl and alert_server

--- cloud/aws/stage3/dev-services/dev_etl/dev_etl.tf
+++ cloud/aws/stage3/dev-services/dev_etl/dev_etl.tf
@@ -48,7 +48,7 @@ variable "local_shell" {}
 
 resource "aws_lambda_function" "dev_etl_lambda_HCGH" {
 
-    function_name    = "${var.deploy_prefix}-etl-lambda-HCGH"
+    function_name    = "${var.deploy_prefix}-dev-etl-lambda-HCGH"
     handler          = "service.handler"
     s3_bucket        = "${var.s3_opsdx_lambda}"
     s3_key           = "${var.aws_klaunch_lambda_package}"
@@ -101,118 +101,118 @@ resource "aws_lambda_function" "dev_etl_lambda_HCGH" {
     }
 }
 
-#resource "aws_lambda_function" "dev_etl_lambda_JHH" {
-#
-#    function_name    = "${var.deploy_prefix}-etl-lambda-JHH"
-#    handler          = "service.handler"
-#    s3_bucket        = "${var.s3_opsdx_lambda}"
-#    s3_key           = "${var.aws_klaunch_lambda_package}"
-#    role             = "${var.aws_klaunch_lambda_role_arn}"
-#    runtime          = "python2.7"
-#    timeout          = 300
-#
-#    environment {
-#      variables {
-#        PYKUBE_KUBERNETES_SERVICE_HOST = "${var.k8s_dev_server_host}"
-#        PYKUBE_KUBERNETES_SERVICE_PORT = "${var.k8s_dev_server_port}"
-#
-#        kube_job_name  = "epic2op-jhh-dev"
-#        kube_nodegroup = "spot-nodes"
-#        kube_cpu_requests = "500m"
-#        kube_mem_requests = "1Gi"
-#        kube_name      = "${var.k8s_dev_name}"
-#        kube_server    = "${var.k8s_dev_server}"
-#        kube_cert_auth = "${var.k8s_dev_cert_auth}"
-#        kube_user      = "${var.k8s_dev_user}"
-#        kube_pass      = "${var.k8s_dev_pass}"
-#        kube_image     = "${var.k8s_dev_image}"
-#        kube_active_deadline_seconds = "300"
-#
-#        kube_cmd_0 = "sh"
-#        kube_cmd_1 = "-c"
-#        kube_cmd_2 = "/usr/local/bin/python3 /etl/epic2op/engine.py --hospital=JHH"
-#
-#        k8s_job_db_host     = "${var.dev_db_host}"
-#        k8s_job_db_port     = "${var.dev_db_port}"
-#        k8s_job_db_name     = "${var.dev_db_name}"
-#        k8s_job_db_user     = "${var.dev_db_username}"
-#        k8s_job_db_password = "${var.dev_db_password}"
-#
-#        k8s_job_jhapi_client_id     = "${var.jhapi_client_id}"
-#        k8s_job_jhapi_client_secret = "${var.jhapi_client_secret}"
-#        k8s_job_etl_channel         = "${var.dev_etl_channel}"
-#
-#        k8s_job_TREWS_ETL_SERVER             = "${var.DEV_ETL_SERVER}"
-#        k8s_job_TREWS_ETL_HOSPITAL           = "JHH"
-#        k8s_job_TREWS_ETL_HOURS              = "${var.DEV_ETL_HOURS}"
-#        k8s_job_TREWS_ETL_ARCHIVE            = "${var.DEV_ETL_ARCHIVE}"
-#        k8s_job_TREWS_ETL_MODE               = "${var.DEV_ETL_MODE}"
-#        k8s_job_TREWS_ETL_STREAM_HOURS       = "${var.DEV_ETL_STREAM_HOURS}"
-#        k8s_job_TREWS_ETL_STREAM_SLICES      = "${var.DEV_ETL_STREAM_SLICES}"
-#        k8s_job_TREWS_ETL_STREAM_SLEEP_SECS  = "${var.DEV_ETL_STREAM_SLEEP_SECS}"
-#        k8s_job_TREWS_ETL_EPIC_NOTIFICATIONS = "${var.DEV_ETL_EPIC_NOTIFICATIONS}"
-#        k8s_job_TREWS_ETL_SUPPRESSION = "1"
-#      }
-#    }
-#}
-#
-#resource "aws_lambda_function" "dev_etl_lambda_BMC" {
-#
-#    function_name    = "${var.deploy_prefix}-etl-lambda-BMC"
-#    handler          = "service.handler"
-#    s3_bucket        = "${var.s3_opsdx_lambda}"
-#    s3_key           = "${var.aws_klaunch_lambda_package}"
-#    role             = "${var.aws_klaunch_lambda_role_arn}"
-#    runtime          = "python2.7"
-#    timeout          = 300
-#
-#    environment {
-#      variables {
-#        PYKUBE_KUBERNETES_SERVICE_HOST = "${var.k8s_dev_server_host}"
-#        PYKUBE_KUBERNETES_SERVICE_PORT = "${var.k8s_dev_server_port}"
-#
-#        kube_job_name  = "epic2op-bmc-dev"
-#        kube_nodegroup = "spot-nodes"
-#        kube_cpu_requests = "250m"
-#        kube_mem_requests = "1Gi"
-#        kube_name      = "${var.k8s_dev_name}"
-#        kube_server    = "${var.k8s_dev_server}"
-#        kube_cert_auth = "${var.k8s_dev_cert_auth}"
-#        kube_user      = "${var.k8s_dev_user}"
-#        kube_pass      = "${var.k8s_dev_pass}"
-#        kube_image     = "${var.k8s_dev_image}"
-#        kube_active_deadline_seconds = "300"
-#
-#        kube_cmd_0 = "sh"
-#        kube_cmd_1 = "-c"
-#        kube_cmd_2 = "/usr/local/bin/python3 /etl/epic2op/engine.py --hospital=BMC"
-#
-#        k8s_job_db_host     = "${var.dev_db_host}"
-#        k8s_job_db_port     = "${var.dev_db_port}"
-#        k8s_job_db_name     = "${var.dev_db_name}"
-#        k8s_job_db_user     = "${var.dev_db_username}"
-#        k8s_job_db_password = "${var.dev_db_password}"
-#
-#        k8s_job_jhapi_client_id     = "${var.jhapi_client_id}"
-#        k8s_job_jhapi_client_secret = "${var.jhapi_client_secret}"
-#        k8s_job_etl_channel         = "${var.dev_etl_channel}"
-#
-#        k8s_job_TREWS_ETL_SERVER             = "${var.DEV_ETL_SERVER}"
-#        k8s_job_TREWS_ETL_HOSPITAL           = "BMC"
-#        k8s_job_TREWS_ETL_HOURS              = "${var.DEV_ETL_HOURS}"
-#        k8s_job_TREWS_ETL_ARCHIVE            = "${var.DEV_ETL_ARCHIVE}"
-#        k8s_job_TREWS_ETL_MODE               = "${var.DEV_ETL_MODE}"
-#        k8s_job_TREWS_ETL_STREAM_HOURS       = "${var.DEV_ETL_STREAM_HOURS}"
-#        k8s_job_TREWS_ETL_STREAM_SLICES      = "${var.DEV_ETL_STREAM_SLICES}"
-#        k8s_job_TREWS_ETL_STREAM_SLEEP_SECS  = "${var.DEV_ETL_STREAM_SLEEP_SECS}"
-#        k8s_job_TREWS_ETL_EPIC_NOTIFICATIONS = "${var.DEV_ETL_EPIC_NOTIFICATIONS}"
-#        k8s_job_TREWS_ETL_SUPPRESSION = "1"
-#      }
-#    }
-#}
+resource "aws_lambda_function" "dev_etl_lambda_JHH" {
+
+    function_name    = "${var.deploy_prefix}-dev-etl-lambda-JHH"
+    handler          = "service.handler"
+    s3_bucket        = "${var.s3_opsdx_lambda}"
+    s3_key           = "${var.aws_klaunch_lambda_package}"
+    role             = "${var.aws_klaunch_lambda_role_arn}"
+    runtime          = "python2.7"
+    timeout          = 300
+
+    environment {
+      variables {
+        PYKUBE_KUBERNETES_SERVICE_HOST = "${var.k8s_dev_server_host}"
+        PYKUBE_KUBERNETES_SERVICE_PORT = "${var.k8s_dev_server_port}"
+
+        kube_job_name  = "epic2op-jhh-dev"
+        kube_nodegroup = "spot-nodes"
+        kube_cpu_requests = "500m"
+        kube_mem_requests = "1Gi"
+        kube_name      = "${var.k8s_dev_name}"
+        kube_server    = "${var.k8s_dev_server}"
+        kube_cert_auth = "${var.k8s_dev_cert_auth}"
+        kube_user      = "${var.k8s_dev_user}"
+        kube_pass      = "${var.k8s_dev_pass}"
+        kube_image     = "${var.k8s_dev_image}"
+        kube_active_deadline_seconds = "300"
+
+        kube_cmd_0 = "sh"
+        kube_cmd_1 = "-c"
+        kube_cmd_2 = "/usr/local/bin/python3 /etl/epic2op/engine.py --hospital=JHH"
+
+        k8s_job_db_host     = "${var.dev_db_host}"
+        k8s_job_db_port     = "${var.dev_db_port}"
+        k8s_job_db_name     = "${var.dev_db_name}"
+        k8s_job_db_user     = "${var.dev_db_username}"
+        k8s_job_db_password = "${var.dev_db_password}"
+
+        k8s_job_jhapi_client_id     = "${var.jhapi_client_id}"
+        k8s_job_jhapi_client_secret = "${var.jhapi_client_secret}"
+        k8s_job_etl_channel         = "${var.dev_etl_channel}"
+
+        k8s_job_TREWS_ETL_SERVER             = "${var.DEV_ETL_SERVER}"
+        k8s_job_TREWS_ETL_HOSPITAL           = "JHH"
+        k8s_job_TREWS_ETL_HOURS              = "${var.DEV_ETL_HOURS}"
+        k8s_job_TREWS_ETL_ARCHIVE            = "${var.DEV_ETL_ARCHIVE}"
+        k8s_job_TREWS_ETL_MODE               = "${var.DEV_ETL_MODE}"
+        k8s_job_TREWS_ETL_STREAM_HOURS       = "${var.DEV_ETL_STREAM_HOURS}"
+        k8s_job_TREWS_ETL_STREAM_SLICES      = "${var.DEV_ETL_STREAM_SLICES}"
+        k8s_job_TREWS_ETL_STREAM_SLEEP_SECS  = "${var.DEV_ETL_STREAM_SLEEP_SECS}"
+        k8s_job_TREWS_ETL_EPIC_NOTIFICATIONS = "${var.DEV_ETL_EPIC_NOTIFICATIONS}"
+        k8s_job_TREWS_ETL_SUPPRESSION = "1"
+      }
+    }
+}
+
+resource "aws_lambda_function" "dev_etl_lambda_BMC" {
+
+    function_name    = "${var.deploy_prefix}-dev-etl-lambda-BMC"
+    handler          = "service.handler"
+    s3_bucket        = "${var.s3_opsdx_lambda}"
+    s3_key           = "${var.aws_klaunch_lambda_package}"
+    role             = "${var.aws_klaunch_lambda_role_arn}"
+    runtime          = "python2.7"
+    timeout          = 300
+
+    environment {
+      variables {
+        PYKUBE_KUBERNETES_SERVICE_HOST = "${var.k8s_dev_server_host}"
+        PYKUBE_KUBERNETES_SERVICE_PORT = "${var.k8s_dev_server_port}"
+
+        kube_job_name  = "epic2op-bmc-dev"
+        kube_nodegroup = "spot-nodes"
+        kube_cpu_requests = "250m"
+        kube_mem_requests = "1Gi"
+        kube_name      = "${var.k8s_dev_name}"
+        kube_server    = "${var.k8s_dev_server}"
+        kube_cert_auth = "${var.k8s_dev_cert_auth}"
+        kube_user      = "${var.k8s_dev_user}"
+        kube_pass      = "${var.k8s_dev_pass}"
+        kube_image     = "${var.k8s_dev_image}"
+        kube_active_deadline_seconds = "300"
+
+        kube_cmd_0 = "sh"
+        kube_cmd_1 = "-c"
+        kube_cmd_2 = "/usr/local/bin/python3 /etl/epic2op/engine.py --hospital=BMC"
+
+        k8s_job_db_host     = "${var.dev_db_host}"
+        k8s_job_db_port     = "${var.dev_db_port}"
+        k8s_job_db_name     = "${var.dev_db_name}"
+        k8s_job_db_user     = "${var.dev_db_username}"
+        k8s_job_db_password = "${var.dev_db_password}"
+
+        k8s_job_jhapi_client_id     = "${var.jhapi_client_id}"
+        k8s_job_jhapi_client_secret = "${var.jhapi_client_secret}"
+        k8s_job_etl_channel         = "${var.dev_etl_channel}"
+
+        k8s_job_TREWS_ETL_SERVER             = "${var.DEV_ETL_SERVER}"
+        k8s_job_TREWS_ETL_HOSPITAL           = "BMC"
+        k8s_job_TREWS_ETL_HOURS              = "${var.DEV_ETL_HOURS}"
+        k8s_job_TREWS_ETL_ARCHIVE            = "${var.DEV_ETL_ARCHIVE}"
+        k8s_job_TREWS_ETL_MODE               = "${var.DEV_ETL_MODE}"
+        k8s_job_TREWS_ETL_STREAM_HOURS       = "${var.DEV_ETL_STREAM_HOURS}"
+        k8s_job_TREWS_ETL_STREAM_SLICES      = "${var.DEV_ETL_STREAM_SLICES}"
+        k8s_job_TREWS_ETL_STREAM_SLEEP_SECS  = "${var.DEV_ETL_STREAM_SLEEP_SECS}"
+        k8s_job_TREWS_ETL_EPIC_NOTIFICATIONS = "${var.DEV_ETL_EPIC_NOTIFICATIONS}"
+        k8s_job_TREWS_ETL_SUPPRESSION = "1"
+      }
+    }
+}
 
 resource "aws_cloudwatch_event_rule" "dev_etl_schedule_rule" {
-    name = "${var.deploy_prefix}-etl-schedule-rule"
+    name = "${var.deploy_prefix}-dev-etl-schedule-rule"
     description = "Fires every ${var.dev_etl_lambda_firing_rate_mins} minutes"
     schedule_expression = "rate(${var.dev_etl_lambda_firing_rate_mins} minutes)"
 }
@@ -223,17 +223,17 @@ resource "aws_cloudwatch_event_target" "dev_etl_schedule_target_HCGH" {
     arn       = "${aws_lambda_function.dev_etl_lambda_HCGH.arn}"
 }
 
-#resource "aws_cloudwatch_event_target" "dev_etl_schedule_target_JHH" {
-#    rule      = "${aws_cloudwatch_event_rule.dev_etl_schedule_rule.name}"
-#    target_id = "${replace(var.deploy_prefix, "-", "_")}_etl_lambda_JHH"
-#    arn       = "${aws_lambda_function.dev_etl_lambda_JHH.arn}"
-#}
+resource "aws_cloudwatch_event_target" "dev_etl_schedule_target_JHH" {
+    rule      = "${aws_cloudwatch_event_rule.dev_etl_schedule_rule.name}"
+    target_id = "${replace(var.deploy_prefix, "-", "_")}_etl_lambda_JHH"
+    arn       = "${aws_lambda_function.dev_etl_lambda_JHH.arn}"
+}
 
-#resource "aws_cloudwatch_event_target" "dev_etl_schedule_target_BMC" {
-#    rule      = "${aws_cloudwatch_event_rule.dev_etl_schedule_rule.name}"
-#    target_id = "${replace(var.deploy_prefix, "-", "_")}_etl_lambda_BMC"
-#    arn       = "${aws_lambda_function.dev_etl_lambda_BMC.arn}"
-#}
+resource "aws_cloudwatch_event_target" "dev_etl_schedule_target_BMC" {
+    rule      = "${aws_cloudwatch_event_rule.dev_etl_schedule_rule.name}"
+    target_id = "${replace(var.deploy_prefix, "-", "_")}_etl_lambda_BMC"
+    arn       = "${aws_lambda_function.dev_etl_lambda_BMC.arn}"
+}
 
 resource "aws_lambda_permission" "dev_etl_cloudwatch_permissions_HCGH" {
     statement_id  = "ETLPeriodicExecution"
@@ -243,18 +243,18 @@ resource "aws_lambda_permission" "dev_etl_cloudwatch_permissions_HCGH" {
     source_arn    = "${aws_cloudwatch_event_rule.dev_etl_schedule_rule.arn}"
 }
 
-#resource "aws_lambda_permission" "dev_etl_cloudwatch_permissions_JHH" {
-#    statement_id  = "ETLPeriodicExecution"
-#    action        = "lambda:InvokeFunction"
-#    function_name = "${aws_lambda_function.dev_etl_lambda_JHH.function_name}"
-#    principal     = "events.amazonaws.com"
-#    source_arn    = "${aws_cloudwatch_event_rule.dev_etl_schedule_rule.arn}"
-#}
-#
-#resource "aws_lambda_permission" "etl_cloudwatch_permissions_BMC" {
-#    statement_id  = "ETLPeriodicExecution"
-#    action        = "lambda:InvokeFunction"
-#    function_name = "${aws_lambda_function.dev_etl_lambda_BMC.function_name}"
-#    principal     = "events.amazonaws.com"
-#    source_arn    = "${aws_cloudwatch_event_rule.dev_etl_schedule_rule.arn}"
-#}
+resource "aws_lambda_permission" "dev_etl_cloudwatch_permissions_JHH" {
+    statement_id  = "ETLPeriodicExecution"
+    action        = "lambda:InvokeFunction"
+    function_name = "${aws_lambda_function.dev_etl_lambda_JHH.function_name}"
+    principal     = "events.amazonaws.com"
+    source_arn    = "${aws_cloudwatch_event_rule.dev_etl_schedule_rule.arn}"
+}
+
+resource "aws_lambda_permission" "etl_cloudwatch_permissions_BMC" {
+    statement_id  = "ETLPeriodicExecution"
+    action        = "lambda:InvokeFunction"
+    function_name = "${aws_lambda_function.dev_etl_lambda_BMC.function_name}"
+    principal     = "events.amazonaws.com"
+    source_arn    = "${aws_cloudwatch_event_rule.dev_etl_schedule_rule.arn}"
+}
