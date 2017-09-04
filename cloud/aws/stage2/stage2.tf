706cf0851d5d49d12aa518a069e33fe4d5c029ca
Yanif Ahmad <yanif@jhu.edu>
2017-09-03 20:13:39 -0400

    More Dev ML cluster updates.

--- cloud/aws/stage2/stage2.tf
+++ cloud/aws/stage2/stage2.tf
@@ -12,6 +12,6 @@ module "k8s_prod" {
 
 module "k8s_dev_ml" {
   source = "./k8s-dev-ml"
-  #node_sg_id = "sg-f7bb9587"
-  #controller_sg_id = "sg-6592cb15"
+  node_sg_id = "sg-c4c4efb4"
+  controller_sg_id = "sg-6592cb15"
 }
