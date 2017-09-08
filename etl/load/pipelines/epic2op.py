d55690214a030ea25879ab7df6874b4b1f6b6e95
Andong Zhan <zad522@gmail.com>
2017-09-08 11:29:20 -0400

    fixed a bug on notify_trews_alert

--- etl/load/pipelines/epic2op.py
+++ etl/load/pipelines/epic2op.py
@@ -70,7 +70,7 @@ async def notify_data_ready_to_lmc_alert_server(ctxt, job_id):
     ctxt.log.exception(e)
     ctxt.log.error("Fail to notify lmc alert server")
 
-async def notify_data_ready_to_trews_alert_server(ctxt, job_id):
+async def notify_data_ready_to_trews_alert_server(ctxt, job_id, _):
   message = {
     'type': 'ETL',
     'time': str(dt.datetime.utcnow()),
