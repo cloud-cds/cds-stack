5e55ae8af9c0f5e693341050ac1134151bdd4d2a
Yanif Ahmad <yanif@jhu.edu>
2017-09-11 11:53:24 -0400

    Added support for copy from query as well as table for dw-s3-export

--- cloud/aws/stage3/jobs/dev/dw-s3-export/export.sh
+++ cloud/aws/stage3/jobs/dev/dw-s3-export/export.sh
@@ -11,11 +11,27 @@ sleep 5
 if [ -d /mnt/$output_dir ]; then
   export PGPASSWORD=$db_password
 
-  # Export tables
-  for i in $export_tables; do
-    echo -n "Exporting $i ... "
-    time (psql -h dw.jh.opsdx.io -U opsdx_root -d opsdx_dev_dw -p 5432 -c "\copy $i to '/mnt/$output_dir/$i.csv' csv delimiter E'\t' quote E'\b'" && echo "[OK]" || echo "[Failed]")
-  done
+  # Export query
+  if [ -n "$export_tables" ]; then
+    # Export tables
+    for i in $export_tables; do
+      echo -n "Exporting $i ... "
+      time (psql -h $db_host -U $db_user -d $db_name -p $db_port -c "\copy $i to '/mnt/$output_dir/$i.csv' csv delimiter E'\t' quote E'\b'" && echo "[OK]" || echo "[Failed]")
+    done
+  elif [ -n "$export_query" ]; then
+    if [ -n "$query_id" ]; then
+      # Export query
+      echo -n "Exporting query ... "
+      time (psql -h $db_host -U $db_user -d $db_name -p $db_port -c "\copy ( $export_query ) to '/mnt/$output_dir/$query_id.csv' csv delimiter E'\t' quote E'\b'" && echo "[OK]" || echo "[Failed]")
+    else
+      echo "A 'query_id' parameter must be given when exporting with a query."
+      exit 1
+    fi
+  else
+    echo "No query or tables specified for exporting."
+    exit 1
+  fi
+
 else
   echo "Failed to find output directory in /mnt/$output_dir"
   exit 1
