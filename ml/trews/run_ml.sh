4680f79c9794b3d7ed19418a19698762ea8ab084
Andong Zhan <zad522@gmail.com>
2017-09-12 16:22:18 -0400

    update ml docker

--- ml/trews/run_ml.sh
+++ ml/trews/run_ml.sh
@@ -3,8 +3,8 @@ echo "start trews-ml"
 echo -n "Mounting Clarity ETL S3 bucket... "
 service rsyslog start
 mkdir mnt
-/bin/goofys opsdx-ml-model mnt
+/bin/goofys opsdx-ml-model /trews/mnt
 sleep 3
 
 echo "run training script"
-python main.py
+python /trews/main.py
