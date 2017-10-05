fd5cb9389ea31c59402189c63214813c3de64170
Andong Zhan <zad522@gmail.com>
2017-10-05 19:01:12 -0400

    try to fix pr ci

--- tests/lite_tests.sh
+++ tests/lite_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Running lite tests"
-pip install --upgrade ./dashan-etl-pr
+pip install --upgrade ./dashan-universe-pr
 #python ./dashan-db/test/run_test_db_compare.py daily_compare_light
