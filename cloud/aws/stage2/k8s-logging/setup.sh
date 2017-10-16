ae438158788e04793a0da6956b550355d306afcd
Yanif Ahmad <yanif@jhu.edu>
2017-10-16 00:05:40 -0400

    Refreshed fluentd daemonset and opsdx-cluster-logging image to fix aws version in fluentd-cloudwatch plugin (see Yanif's forked fluentd-cloudwatch plugin repo)

--- cloud/aws/stage2/k8s-logging/setup.sh
+++ cloud/aws/stage2/k8s-logging/setup.sh
@@ -4,7 +4,7 @@ git clone https://github.com/yanif/fluent-plugin-cloudwatch-logs.git
 cd fluent-plugin-cloudwatch-logs
 gem install bundler
 rake build
-gem install pkg/fluent-plugin-cloudwatch-logs-0.3.7.pre.gem
+gem install pkg/fluent-plugin-cloudwatch-logs-0.4.1.gem
 gem uninstall bundler -a -x
 cd ..
 rm -rf fluent-plugin-cloudwatch-logs
