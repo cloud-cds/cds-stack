a10a38f0a4530b0c9aa6a65f6cd63d8a80c273f8
noam finkelstein <noam.finkelstein@gmail.com>
2017-11-06 08:41:08 -0500

    Bash script

--- etl/opsdx2cardiac/transfer_namespace.sh
+++ etl/opsdx2cardiac/transfer_namespace.sh
@@ -1,30 +1,33 @@
+# open ssh tunnel through opsdx dev controller
 ssh -i ~/keys/tf-opsdx -N -p 22 ubuntu@controller.jh.opsdx.io -L localhost:5432:dw.jh.opsdx.io:5432
-os.environ['PGPASSWORD'] = opsdx_password
-dump = "pg_dump -h {h} -U opsdx_root --schema={n} -d opsdx_dev_dw > {d}{n}.dump"
-os.system(dump.format_map({'h': host, 'n': namespace, 'd': datadir}))
 
-os.environ['PGPASSWORD'] = cardiac_password
-restore = "pg_restore -h {h} -U opsdx_root -d cardiac_db_small {d}{n}.dump"
-os.system(restore.format_map({'h': host, 'n': namespace, 'd': datadir}))
+$schema=clarity_1m
+$dataset_id=2
+
+# dump relevant schema from opsdx_dev_dw
+pg_dump -h dw.jh.opsdx.io -U opsdx_root --schema=schema -d opsdx_dev_dw > `$schema`.dump
+
+# restore schema to cardiac_db_small
+pg_restore -h {h} -U opsdx_root -d cardiac_db_small {d}{n}.dump
 	
-os.environ['db_password'] = cardiac_password
-os.environ['clarity_workspace'] = namespace
-os.environ['dataset_is'] = str(dataset_id)
-os.environ['nprocs'] = nprocs
-os.environ['num_derive_groups'] = "8"
+export db_password=cardiac_password
+export clarity_workspace=namespace
+export dataset_is=str(dataset_id)
+export nprocs=nprocs
+export num_derive_groups="8"
 
-os.environ['min_tsp'] = '1990-01-01'
-os.environ['vacuum_temp_table'] = 'True'
-os.environ['db_host'] = 'dw.jh.opsdx.io'
-os.environ['db_port'] = '5432'
-os.environ['db_name'] = 'cardiac_db_small'
+export min_tsp='1990-01-01'
+export vacuum_temp_table='True'
+export db_host='dw.jh.opsdx.io'
+export db_port='5432'
+export db_name='cardiac_db_small'
 
-os.environ['feature_mapping'] = "feature_mapping.csv,feature_mapping_cardiac.csv"
+export feature_mapping="feature_mapping.csv,feature_mapping_cardiac.csv"
 
-os.environ['offline_criteria_processing'] = 'False'
-os.environ['extract_init'] = 'True'
-os.environ['populate_patients'] = 'True'
-os.environ['fillin'] = 'True'
-os.environ['derive'] = 'True'
+export offline_criteria_processing='False'
+export extract_init='True'
+export populate_patients='True'
+export fillin='True'
+export derive='True'
 
 python ../etl/clarity2dw/planner.py
