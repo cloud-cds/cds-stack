37627f98d1d7b11855a2e82d678ba88057ee0194
noam finkelstein <noam.finkelstein@gmail.com>
2017-11-06 09:15:41 -0500

    Bash

--- etl/opsdx2cardiac/transfer_namespace.sh
+++ etl/opsdx2cardiac/transfer_namespace.sh
@@ -1,19 +1,20 @@
 # open ssh tunnel through opsdx dev controller
-ssh -i ~/keys/tf-opsdx -N -p 22 ubuntu@controller.jh.opsdx.io -L localhost:5432:dw.jh.opsdx.io:5432
+ssh -i ~/keys/tf-opsdx -N -p 22 ubuntu@controller.jh.opsdx.io -L localhost:5432:dw.jh.opsdx.io:5432 &
+TUNNEL_PID=$!
 
-$schema=clarity_1m
-$dataset_id=2
+# mount rambo for space on disk
+sshfs -o IdentityFile=~/keys/noam_rsa noam@rambo.isi.jhu.edu:/udata/noam ~/mnt/rambo/ &
+MOUNT_ID=$!
 
 # dump relevant schema from opsdx_dev_dw
-pg_dump -h dw.jh.opsdx.io -U opsdx_root --schema=schema -d opsdx_dev_dw > `$schema`.dump
+pg_dump -h 127.0.0.1 -U opsdx_root --schema=schema -d opsdx_dev_dw > ~/mnt/rambo/$1.dump
 
 # restore schema to cardiac_db_small
-pg_restore -h {h} -U opsdx_root -d cardiac_db_small {d}{n}.dump
-	
-export db_password=cardiac_password
-export clarity_workspace=namespace
-export dataset_is=str(dataset_id)
-export nprocs=nprocs
+pg_restore -h 127.0.0.1 -U opsdx_root -d cardiac_db_small ~/mnt/rambo/$1.dump
+
+export clarity_workspace=$1
+export dataset_is=$2
+export nprocs="8"
 export num_derive_groups="8"
 
 export min_tsp='1990-01-01'
@@ -31,3 +32,9 @@ export fillin='True'
 export derive='True'
 
 python ../etl/clarity2dw/planner.py
+
+# close tunnel
+kill $TUNNEL_PID
+
+# unmount rambo
+kill $MOUNT_ID
