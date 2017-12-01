e6029fb6d976859cf834cf7d9b64c391f6456895
Andong Zhan <zad522@gmail.com>
2017-11-30 21:49:16 -0500

    forward message

--- etl/events-soap/server.js
+++ etl/events-soap/server.js
@@ -10,7 +10,21 @@ var service = {
     EventService : {
         EventPort : {
             ProcessEvent:function(args){
-                console.log(args);
+                console.log('new event message:');
+                console.log(JSON.stringify(args, null, 4));
+                var request = require('request');
+
+                var options = {
+                  uri: 'https://trews-dev.jh.opsdx.io/event',
+                  method: 'POST',
+                  json: args
+                };
+
+                request(options, function (error, response, body) {
+                  if (!error && response.statusCode == 200) {
+                    console.log(body.id) // Print the shortened url.
+                  }
+                });
                 return {};
             }
         }
