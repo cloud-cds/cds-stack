95d9eab7d1800b434d0d2f3450132562a2733457
Andong Zhan <zad522@gmail.com>
2018-01-08 16:15:36 -0500

    enable event forward

--- etl/events-soap/server.js
+++ etl/events-soap/server.js
@@ -13,8 +13,7 @@ var myMetric = new cloudwatchMetrics.Metric('OpsDX', 'Count', [{
     console.log(err)
   }
 });
-var count = 0;
-var max_count = parseInt(process.env.max_count);
+var event_forward = process.env.event_forward;
 /**
 -this is remote service defined in this file, that can be accessed by clients, who will supply args
 -response is returned to the calling client
@@ -28,25 +27,20 @@ var service = {
                 console.log(JSON.stringify(args, null, 4));
                 //var request = require('request');
 
-                // TODO: enable event forwarding and use the right URL
-                // var options = {
-                //   uri: 'https://event-dev.jh.opsdx.io',
-                //   method: 'POST',
-                //   json: args
-                // };
+                if(event_forward != ''){
+                    // enable event forwarding and use the right URL
+                    var options = {
+                      uri: 'https://' + event_forward,
+                      method: 'POST',
+                      json: args
+                    };
 
-                // request(options, function (error, response, body) {
-                //   if (!error && response.statusCode == 200) {
-                //     console.log(body.id) // Print the shortened url.
-                //   }
-                // });
-
-                // count = count + 1;
-                // if(count >= max_count)
-                // {
-                //   myMetric.put(count, 'EventCount');
-                //   count = 0;
-                // }
+                    request(options, function (error, response, body) {
+                      if (!error && response.statusCode == 200) {
+                        console.log(body.id) // Print the shortened url.
+                      }
+                    });
+                }
                 myMetric.put(1, 'EventCount');
                 return {};
             }
