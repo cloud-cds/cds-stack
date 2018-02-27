fdb01068951e8d174128b1acee19e295f79f2842
hosein.soleimani@gmail.com <hosein.soleimani@gmail.com>
2018-02-27 22:47:37 +0000

    adding the new proc_mapping file

---
+++ etl/mappings/active_procedure_names.py
@@ -0,0 +1,8 @@
+active_procedure_names = [
+    { 'fid':'consult', 'pos':'consult'},
+    { 'fid':'ircv_case_request', 'pos':'case request ircv lab'},
+    { 'fid':'angiogram_order', 'pos':'angiogram', 'neg':'case request ircv lab'},
+    { 'fid':'mech_vent', 'pos':'mechanical ventilation', 'neg':'cpap|bipap'},
+    { 'fid':'bipap', 'pos':'bipap'},
+    { 'fid':'cpap', 'pos':'cpap'}
+]
