4243cd9b2e878f16a251d05afb2d202d71e41dce
Andong Zhan <zad522@gmail.com>
2018-04-26 09:08:49 -0400

    remove ml related code

--- ml/lmc/run-ml.sh
+++
@@ -1,7 +0,0 @@
-echo "start trews-ml"
-echo "installing R packages"
-R CMD BATCH /ml/install-packages.R
-echo "R packages installed"
-alias python=python3.6
-python ml/generate_input.py
-python ml/main.py input/$dataset_id.pkl
